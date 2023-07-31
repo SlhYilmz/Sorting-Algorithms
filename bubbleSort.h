1c41910db78c400fd33bf16e30cd7fd3bb7a7d80
Muhammed Salih Yılmaz <msy-2000@hotmail.com>
2023-07-31 10:48:40 +0300

    Bubble Sort implemented.

---
+++ bubbleSort.h
@@ -0,0 +1,30 @@
+#ifndef BUBBLESORT_H
+#define BUBBLESORT_H
+
+#include <stdbool.h>
+#include "helperFunctions.h"
+
+// An optimized version of Bubble Sort
+void bubbleSort(int arr[], int n)
+{
+    bool swapped;
+    for (int i = 0; i < n - 1; i++)
+    {
+        swapped = false;
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(&arr[j], &arr[j + 1]);
+                swapped = true;
+            }
+        }
+
+        // If no two elements were swapped by inner loop,
+        // then break
+        if (swapped == false)
+            break;
+    }
+}
+
+#endif // BUBBLESORT_H
---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "bubbleSort.h"
+
+
+int main()
+{
+    int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    bubbleSort(arr, n);
+    printf("Sorted array: \n");
+    printArray(arr, n);
+    return 0;
+}
