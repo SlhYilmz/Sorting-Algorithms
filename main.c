91d6c26d3d7f2815e21d107e72008c56eac9d636
Muhammed Salih Yılmaz <msy-2000@hotmail.com>
2023-07-31 11:05:29 +0300

    Bubble sort updated.

--- main.c
+++ main.c
@@ -2,13 +2,18 @@
 #include <stdlib.h>
 #include "bubbleSort.h"
 
-
 int main()
 {
-    int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
-    int n = sizeof(arr) / sizeof(arr[0]);
-    bubbleSort(arr, n);
+    int arr1[] = {64, 34, 25, 12, 22, 90, 11};
+    int size1 = sizeof(arr1) / sizeof(arr1[0]);
+    printf("\n");
+    printf("-----------BUBBLE SORT-----------\n");
+    printf("Unsorted array: \n");
+    printArray(arr1, size1);
+
+    bubbleSort(arr1, size1);
     printf("Sorted array: \n");
-    printArray(arr, n);
+    printArray(arr1, size1);
+
     return 0;
 }
