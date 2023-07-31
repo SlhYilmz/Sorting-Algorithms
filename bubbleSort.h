c73876de264111f5083e287150189578432f5289
Muhammed Salih Yılmaz <msy-2000@hotmail.com>
2023-07-31 11:03:26 +0300

    Bubble sort updated

--- bubbleSort.h
+++ bubbleSort.h
@@ -5,25 +5,19 @@
 #include "helperFunctions.h"
 
 // An optimized version of Bubble Sort
-void bubbleSort(int arr[], int n)
+void bubbleSort(int arr[], int size)
 {
-    bool swapped;
-    for (int i = 0; i < n - 1; i++)
+    for (int i = 0; i < size - 1; i++)
     {
-        swapped = false;
-        for (int j = 0; j < n - i - 1; j++)
+        for (int j = 0; j < size - i - 1; j++)
         {
             if (arr[j] > arr[j + 1])
             {
                 swap(&arr[j], &arr[j + 1]);
-                swapped = true;
+                
             }
         }
 
-        // If no two elements were swapped by inner loop,
-        // then break
-        if (swapped == false)
-            break;
     }
 }
 
